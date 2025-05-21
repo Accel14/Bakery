e0a4fe7852d8254276b2bc41ea29adb82af2f7a6
accel <faralmtop@gmail.com>
2025-05-21 12:55:15 +0500

    refactor everything + order logic

--- js/order.js
+++ js/order.js
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
+
 function removeFromCart(productId) {
   let cart = JSON.parse(localStorage.getItem('cart')) || {};
   //let newValue = parseInt(document.getElementById("count_" + productId).innerHTML.split(' ')[0]) - 1;
@@ -27,7 +28,7 @@ function removeFromCart(productId) {
 function loadCart() {
 
   const cart = JSON.parse(localStorage.getItem('cart')) || {};
-  if (Object.keys(cart).length > 0) { document.getElementById('empty-cart').remove();}
+  if (Object.keys(cart).length > 0) { document.getElementById('empty-cart').remove(); }
   const productIds = Object.keys(cart).map(Number); // [123, 456, 789]
   const url = `http://localhost:3000/cart?productIds=${productIds}`;
   fetch(url)
@@ -35,21 +36,42 @@ function loadCart() {
     .then(goods => {
       const container = document.getElementById('order-list');
       container.innerHTML = '';
+      let totalCost = null;
+      let totalMass = null;
+      let totalCount = null;
       goods.forEach(product => {
         const el = document.createElement('div');
+        totalCost += parseFloat(product.price);
+        totalMass += parseFloat(product.mass);
+        totalCount += parseFloat(cart[product.id]);
         el.classList.add('order');
         el.id = "order_" + product.id;
         el.innerHTML = `
-            <img src="food/${product.image_path}" class="food-image" alt="${product.name}">
-            <h3>${product.name}</h3>
-            <p>${product.mass} грамм</p>
-            <div class="count" style="display: flex;">
-              <p id="count_${product.id}" value=${cart[product.id]} >${cart[product.id]} шт.</p>
-              <img onclick="removeFromCart(${product.id})" alt="Удалить" style="top: 0; width: 16px; height: 16px" src="img/remove.png"></img>
-            </div>
-            <strong>${product.price} руб.</strong>
-          `;
+          <img src="food/${product.image_path}" class="food-image" alt="${product.name}">
+          <h3>${product.name}</h3>
+          <p>${product.mass} гр.</p>
+          <div class="count">
+            <p id="count_${product.id}" value=${cart[product.id]}>${cart[product.id]} шт.</p>
+            <img onclick="removeFromCart(${product.id})" alt="Удалить" class="remove-icon" src="img/remove.png">
+          </div>
+          <strong>${product.price} руб.</strong>
+        `;
+
         container.appendChild(el);
       });
+
+      const total = document.createElement('div');
+      total.classList.add('order');
+      total.id = 'total-order';
+      total.innerHTML = `
+          <div></div>
+          <h3>Итого</h3>
+          <p id="total-mass">${totalMass} гр.</p>
+          <div class="count">
+            <p id="total-products">${totalCount} шт.</p>
+          </div>
+          <strong id="total-price">${totalCost} руб.</strong>
+        `;
+      container.appendChild(total);
     });
 }
